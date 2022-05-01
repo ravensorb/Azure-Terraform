3467bf640ae32f98e634f4a2a8f4227bc6ba1d2f
Shawn Anderson <sanderson@eye-catcher.com>
2022-05-01 10:24:34 -0400

    Added new hub module and started refactoring

--- Modules/Landing Zone/Hub-Spoke/variables.tf
+++ Modules/Landing Zone/Hub-Spoke/variables.tf
@@ -1,4 +1,37 @@
-variable "prefix" {
-    description = "customer prefix"
-    default = "jvn"
+variable "deploymentDetails" {
+    description = "Details on this specific deployment"
+    type = object({
+        env = string
+        locationPrefix = string
+        instanceNumber = string 
+    })
+    default = {
+      env = "prod"
+      instanceNumber = "001"
+      locationPrefix = "eus2"
+    }
+}
+
+variable "tagsStandard" {
+  description = "standard tags for objects created"
+  type = object({
+      createdBy = string
+      createdOn = string
+      costCenter = string
+      environment = string
+      critical = string
+  })
+  default = {
+    costCenter = "IT"
+    createdBy = "Shawn Anderson"
+    createdOn = formatdate("YYYY/MM/DD", timestamp())
+    critical = "YES"
+    environment = "PROD"
+  }
+}
+
+variable "dnsServers" {
+    description = "list of dns servers for vnets"
+    type = list(string)
+    default = [ "10.5.0.4","168.63.129.16" ]
 }
